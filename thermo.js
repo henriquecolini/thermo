3cad2c63134b4fc6c5cb9d5c5ec12f3352dc2d8a
Henrique <henriquecolini@gmail.com>
2019-09-13 16:49:39 -0300

    Temperature Engine now dynamic

--- thermo.js
+++ thermo.js
@@ -2,14 +2,15 @@ var txtWidth = document.getElementById("txtWidth");
 var txtHeight = document.getElementById("txtHeight");
 var btnReset = document.getElementById("btnReset");
 var canvas = document.getElementById("canvas");
+var labelTemperature = document.getElementById("labelTemperature");
 var ctx = canvas.getContext("2d");
 var tileWitdh = 16;
 var tileHeight = 16;
 var witdh = 1;
 var height = 1;
 function temperatureColor(temperature) {
-    var L = 1;
-    var K = 0.004;
+    var L = 2;
+    var K = 0.002;
     var factor = (L / (1 + (Math.pow(Math.E, (-K * (temperature)))))) - L / 2;
     return "#" + (lerpColor(0x33224d, 0xfabb3e, factor).toString(16));
 }
@@ -18,12 +19,36 @@ function lerpColor(a, b, amount) {
     return (rr << 16) + (rg << 8) + (rb | 0);
 }
 var Tile = (function () {
-    function Tile(density, temperature) {
+    function Tile(density, temperature, entropyRate) {
         this.baseDensity = density;
         this.temperature = temperature;
+        this.entropyRate = entropyRate;
     }
+    Tile.prototype.tick = function (worldResult, x, y) {
+        var next = this.clone();
+        var directions = [];
+        if (y > 0)
+            directions.push({ tile: world[x][y - 1], xOff: 0, yOff: -1 });
+        if (y < height - 1)
+            directions.push({ tile: world[x][y + 1], xOff: 0, yOff: +1 });
+        if (x > 0)
+            directions.push({ tile: world[x - 1][y], xOff: -1, yOff: 0 });
+        if (x < witdh - 1)
+            directions.push({ tile: world[x + 1][y], xOff: +1, yOff: 0 });
+        for (var i = 0; i < directions.length; i++) {
+            var dir = directions[i];
+            if (this.temperature > dir.tile.temperature) {
+                var transferred = ((dir.tile.entropyRate + this.entropyRate) / 2) * (this.temperature - dir.tile.temperature);
+                world[x + dir.xOff][y + dir.yOff].temperature += transferred;
+                world[x][y].temperature -= transferred;
+            }
+        }
+    };
     Tile.prototype.getColor = function () {
         return temperatureColor(this.temperature);
+    };
+    Tile.prototype.clone = function () {
+        return new Tile(this.baseDensity, this.temperature, this.entropyRate);
     };
     return Tile;
 }());
@@ -32,14 +57,35 @@ function generate() {
     world = [];
     for (var x = 0; x < witdh; x++) {
         world[x] = [];
-        for (var y = 0; y < witdh; y++) {
-            world[x][y] = new Tile(0.1, (Math.random() * 800));
+        for (var y = 0; y < height; y++) {
+            world[x][y] = new Tile(0.1, (Math.random() * 600), 0.008);
         }
     }
 }
+function tick() {
+    var worldResult = [];
+    var totalTemperature = 0;
+    for (var x = 0; x < witdh; x++) {
+        worldResult[x] = [];
+        for (var y = 0; y < height; y++) {
+            worldResult[x][y] = world[x][y].clone();
+        }
+    }
+    for (var x = 0; x < witdh; x++) {
+        for (var y = 0; y < height; y++) {
+            world[x][y].tick(worldResult, x, y);
+        }
+    }
+    for (var x = 0; x < witdh; x++) {
+        for (var y = 0; y < height; y++) {
+            totalTemperature += worldResult[x][y].temperature;
+        }
+    }
+    labelTemperature.innerHTML = "Average Temperature = " + (totalTemperature / (witdh * height)).toFixed(4) + " K";
+}
 function draw() {
     for (var x = 0; x < witdh; x++) {
-        for (var y = 0; y < witdh; y++) {
+        for (var y = 0; y < height; y++) {
             ctx.fillStyle = world[x][y].getColor();
             ctx.fillRect(x * tileWitdh, y * tileHeight, tileWitdh, tileHeight);
         }
@@ -54,4 +100,8 @@ btnReset.addEventListener("click", function () {
     draw();
 });
 generate();
+setInterval(function () {
+    tick();
+    draw();
+}, 100);
 //# sourceMappingURL=thermo.js.map
--- thermo.ts
+++ thermo.ts
@@ -2,6 +2,7 @@ const txtWidth = document.getElementById("txtWidth") as HTMLInputElement;
 const txtHeight = document.getElementById("txtHeight") as HTMLInputElement;
 const btnReset = document.getElementById("btnReset") as HTMLButtonElement;
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const labelTemperature = document.getElementById("labelTemperature") as HTMLElement;
 const ctx = canvas.getContext("2d");
 
 const tileWitdh = 16;
@@ -11,8 +12,8 @@ let witdh = 1;
 let height = 1;
 
 function temperatureColor(temperature: number): string {
-	const L = 1;
-	const K = 0.004;
+	const L = 2;
+	const K = 0.002;
 	let factor = (L/(1+(Math.E**(-K*(temperature)))))-L/2;
 	return "#"+(lerpColor(0x33224d, 0xfabb3e, factor).toString(16));
 }
@@ -35,16 +36,46 @@ function lerpColor(a: number, b: number, amount: number): number {
 
 class Tile {
 
-	private baseDensity: number;
-	private temperature: number;
+	public baseDensity: number;
+	public temperature: number;
+	public entropyRate: number
 	
-	constructor(density: number, temperature: number) {
+	constructor(density: number, temperature: number, entropyRate: number) {
 		this.baseDensity = density;
 		this.temperature = temperature;
+		this.entropyRate = entropyRate;
+	}
+
+	public tick(worldResult: Tile[][], x: number, y: number): void {
+
+		const next = this.clone();
+		const directions: {tile: Tile, xOff: number, yOff: number}[] = [];
+		
+		if(y > 0)        directions.push({tile: world[x][y-1], xOff: 0, yOff: -1});
+		if(y < height-1) directions.push({tile: world[x][y+1], xOff: 0, yOff: +1});
+		if(x > 0)        directions.push({tile: world[x-1][y], xOff: -1, yOff: 0});
+		if(x < witdh-1)  directions.push({tile: world[x+1][y], xOff: +1, yOff: 0});
+
+		for (let i = 0; i < directions.length; i++) {
+
+			const dir = directions[i];			
+			
+			if (this.temperature > dir.tile.temperature) {
+				const transferred = ((dir.tile.entropyRate + this.entropyRate)/2) * (this.temperature - dir.tile.temperature);
+				world[x+dir.xOff][y+dir.yOff].temperature += transferred;				
+				world[x][y].temperature -= transferred;				
+			}
+
+		}
+
 	}
 
 	public getColor(): string {
 		return temperatureColor(this.temperature);
+	}
+
+	public clone(): Tile {
+		return new Tile(this.baseDensity, this.temperature, this.entropyRate);
 	}
 	
 }
@@ -57,17 +88,47 @@ function generate() {
 
 	for (let x=0; x<witdh; x++) {
 		world[x] = [];
-		for (let y=0; y<witdh;y++){
-			world[x][y] = new Tile(0.1,(Math.random() * 800));
+		for (let y=0; y<height;y++){
+			world[x][y] = new Tile(0.1,(Math.random() * 600), 0.008);
 		}
 	}
+
+}
+
+function tick() {
+
+	const worldResult: Tile[][] = [];
+	let totalTemperature = 0;
+
+	for (let x=0; x<witdh; x++) {
+		worldResult[x] = [];
+		for (let y=0; y<height;y++){			
+			worldResult[x][y] = world[x][y].clone();
+		}
+	}
+
+	for (let x=0; x<witdh; x++) {
+		for (let y=0; y<height;y++){			
+			world[x][y].tick(worldResult, x, y);
+		}
+	}
+
+	for (let x=0; x<witdh; x++) {
+		for (let y=0; y<height;y++){			
+			totalTemperature += worldResult[x][y].temperature;
+		}
+	}
+
+	labelTemperature.innerHTML = "Average Temperature = " + (totalTemperature/(witdh*height)).toFixed(4) + " K";
+
+	//world = worldResult;
 
 }
 
 function draw() {
 
 	for (let x=0; x<witdh; x++) {
-		for (let y=0; y<witdh;y++){
+		for (let y=0; y<height;y++){
 			ctx.fillStyle = world[x][y].getColor();
 			ctx.fillRect(x*tileWitdh, y*tileHeight, tileWitdh, tileHeight);
 		}
@@ -89,3 +150,8 @@ btnReset.addEventListener("click", () => {
 });
 
 generate();
+
+setInterval(() => {
+	tick();
+	draw();
+}, 100);
