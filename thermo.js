6d4bc4c3459fba8478b9b0dff9156aad4c645078
henriquecolini <henriquecolini@gmail.com>
2019-09-13 12:44:38 -0300

    Very basic temperature system

--- thermo.js
+++ thermo.js
@@ -7,10 +7,24 @@ var tileWitdh = 16;
 var tileHeight = 16;
 var witdh = 1;
 var height = 1;
+function temperatureColor(temperature) {
+    var L = 1;
+    var K = 0.004;
+    var factor = (L / (1 + (Math.pow(Math.E, (-K * (temperature)))))) - L / 2;
+    return "#" + (lerpColor(0x33224d, 0xfabb3e, factor).toString(16));
+}
+function lerpColor(a, b, amount) {
+    var ar = a >> 16, ag = a >> 8 & 0xff, ab = a & 0xff, br = b >> 16, bg = b >> 8 & 0xff, bb = b & 0xff, rr = ar + amount * (br - ar), rg = ag + amount * (bg - ag), rb = ab + amount * (bb - ab);
+    return (rr << 16) + (rg << 8) + (rb | 0);
+}
 var Tile = (function () {
-    function Tile(color) {
-        this.color = color;
+    function Tile(density, temperature) {
+        this.baseDensity = density;
+        this.temperature = temperature;
     }
+    Tile.prototype.getColor = function () {
+        return temperatureColor(this.temperature);
+    };
     return Tile;
 }());
 var world;
@@ -19,14 +33,14 @@ function generate() {
     for (var x = 0; x < witdh; x++) {
         world[x] = [];
         for (var y = 0; y < witdh; y++) {
-            world[x][y] = new Tile("gray");
+            world[x][y] = new Tile(0.1, (Math.random() * 800));
         }
     }
 }
 function draw() {
     for (var x = 0; x < witdh; x++) {
         for (var y = 0; y < witdh; y++) {
-            ctx.fillStyle = world[x][y].color;
+            ctx.fillStyle = world[x][y].getColor();
             ctx.fillRect(x * tileWitdh, y * tileHeight, tileWitdh, tileHeight);
         }
     }
--- thermo.ts
+++ thermo.ts
@@ -10,13 +10,43 @@ const tileHeight = 16;
 let witdh = 1;
 let height = 1;
 
+function temperatureColor(temperature: number): string {
+	const L = 1;
+	const K = 0.004;
+	let factor = (L/(1+(Math.E**(-K*(temperature)))))-L/2;
+	return "#"+(lerpColor(0x33224d, 0xfabb3e, factor).toString(16));
+}
+
+function lerpColor(a: number, b: number, amount: number): number {
+    const ar = a >> 16,
+          ag = a >> 8 & 0xff,
+          ab = a & 0xff,
+
+          br = b >> 16,
+          bg = b >> 8 & 0xff,
+          bb = b & 0xff,
+
+          rr = ar + amount * (br - ar),
+          rg = ag + amount * (bg - ag),
+          rb = ab + amount * (bb - ab);
+
+    return (rr << 16) + (rg << 8) + (rb | 0);
+}
+
 class Tile {
 
-	public color: string;
+	private baseDensity: number;
+	private temperature: number;
 	
-	constructor(color: string) {
-		this.color = color;
+	constructor(density: number, temperature: number) {
+		this.baseDensity = density;
+		this.temperature = temperature;
 	}
+
+	public getColor(): string {
+		return temperatureColor(this.temperature);
+	}
+	
 }
 
 let world: Tile[][];
@@ -28,7 +58,7 @@ function generate() {
 	for (let x=0; x<witdh; x++) {
 		world[x] = [];
 		for (let y=0; y<witdh;y++){
-			world[x][y] = new Tile("gray");
+			world[x][y] = new Tile(0.1,(Math.random() * 800));
 		}
 	}
 
@@ -38,7 +68,7 @@ function draw() {
 
 	for (let x=0; x<witdh; x++) {
 		for (let y=0; y<witdh;y++){
-			ctx.fillStyle = world[x][y].color;
+			ctx.fillStyle = world[x][y].getColor();
 			ctx.fillRect(x*tileWitdh, y*tileHeight, tileWitdh, tileHeight);
 		}
 	}
